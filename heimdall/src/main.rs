805aeaf0b7532d37a8449d3d9c8334496f6725b5
topjohnwu <topjohnwu@gmail.com>
2026-05-19 21:47:12 -0700

    Remove unused extern crate

--- heimdall/src/main.rs
+++ heimdall/src/main.rs
@@ -14,8 +14,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-extern crate libpit;
-
 mod bridge_manager;
 mod detect;
 mod download_pit;
